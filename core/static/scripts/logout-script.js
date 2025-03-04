58d11896fdb5a5c032719eeea4e4ff955e9b3654
Roberto Jr <contatorj.roberto@gmail.com>
2025-03-04 18:09:35 -0300

    Resolvendo conflitos

---
+++ core/static/scripts/logout-script.js
@@ -0,0 +1,18 @@
+document.getElementById("logout-btn").addEventListener("click", async function () {
+        try {
+            let response = await fetch("/api/logout/", {
+                method: "POST",
+                credentials: "include",  // Inclui cookies na requisição
+            });
+
+            if (!response.ok) {
+                throw new Error("Erro ao deslogar");
+            }
+
+            let data = await response.json();
+            window.location.href = "/login_user/";  // Redireciona para login
+        } catch (error) {
+            alert("Erro ao deslogar. Tente novamente!");
+            console.error("Erro no logout:", error);
+        }
+    });
